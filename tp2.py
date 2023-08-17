b7eba36440e157d533ff2db37a66d0d48efaaf2c
JoacoParellada <141734716+JoacoParellada@users.noreply.github.com>
2023-08-17 09:48:54 -0300

    Add files via upload

---
+++ tp2.py
@@ -0,0 +1,62 @@
+"""""  1-4
+
+numero1=20
+numero2=17.5
+suma=numero1+numero2
+resta=numero1-numero2
+multip=numero1*numero2
+div=numero1+numero2
+print(f"SUMA: {suma}")
+print(f"RESTA: {resta}")
+print(f"MULTIPLICACIÓN: {multip}")
+print(f"DIVISIÓN: {div}")
+
+"""
+""" 5-8
+
+
+nombre="Joaquín"
+precio=25.99
+descuento=0.15
+desc_aplicado=precio*descuento
+print(nombre)
+print("El precio original del producto es: $",precio)
+print("El precio del producto con descuento aplicado es de : $",precio-desc_aplicado)
+
+"""
+""" 9-11
+
+cadena="Programación"
+longitud=len(cadena)
+precio=int(20.23)
+print("Cadena: ",cadena)
+print("Longitud: ",longitud)
+print("Precio: ",precio)
+"""
+""" 12
+nombre="Joaquín"
+apellido="Parellada"
+nombre_completo=nombre +" " + apellido
+print("Nombre completo:",nombre_completo)
+"""
+""" 13
+edad=19
+edad+=5
+print(edad)
+edad-=10
+print(edad)
+"""
+"""14
+altura=1.73
+altura*=4
+print(altura)
+altura/=3
+print(altura)
+"""
+"""
+nombre="JOAQUÍN"
+nombre=nombre.lower()
+print(nombre)
+nombre_cap=nombre.capitalize()
+print(nombre_cap)
+"""
